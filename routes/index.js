00914eabafb505999d789c3979b23a66ef620812
James McCormack <thejamesmccormack@gmail.com>
2017-10-05 16:39:04 -0700

    set up cheerio inside of post request

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
 'use strict'
 const router = require('express').Router()
+const cheerio = require('cheerio');
+const rp = require('request-promise');
 
 router.get('/', (req, res) => {
-  res.render('index.pug', {title: 'home'})
+  res.render('index.pug')
 })
+
+router.post('/', (req, res) => {
+  const search = req.body.search;
+  const options = {
+    uri: `http://www.imdb.com/find?ref_=nv_sr_fn&q=${search}&s=all`,
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+
+  rp(options)
+  .then(function ($) {
+    $('small').remove()
+    console.log($('.findList')
+    .first()
+    .find('.result_text')
+    .map((index, element) => {
+      return $(element).text().trim()
+    })
+    .get()
+  )
+})
+.catch(function (err) {
+  console.log(err);
+});
+})
+
+
+
+
 
 module.exports = router;
